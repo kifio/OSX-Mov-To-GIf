9c07aedfda13ecdf7de478a685f06999f4fa46a4
kifio <kg.of.morphine@gmail.com>
2020-04-02 23:02:06 +0200

    Initial Commit

---
+++ MovToGifConverter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MovToGifConverter
+//
+//  Created by imurashov private on 02.04.20.
+//  Copyright © 2020 imurashov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
